f4eedd868811921cca9b2d3b69600b07991838db
Christian Brown <robrowno@icloud.com>
2022-11-29 23:46:47 +0000

    created base template and added block tags and messages for later development

--- django_raceway/settings.py
+++ django_raceway/settings.py
@@ -65,7 +65,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', #required by allauth
+                'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
