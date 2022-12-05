f746b4ea932fd94b57dbd79bf98bf2b33a615841
Christian Brown <robrowno@icloud.com>
2022-12-05 10:58:59 +0000

    fix allowed hosts typo

--- django_raceway/settings.py
+++ django_raceway/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django_raceway.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['django-raceway.herokuapp.com', 'localhost']
 
 
 # Application definition
