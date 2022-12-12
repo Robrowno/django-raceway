f473a9452e229e1c632929f1feeda81bb112dd01
Christian Brown <robrowno@icloud.com>
2022-12-12 19:47:22 +0000

    create static track list page for rendering, create cars app for future dev work

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def car_hire(request):
+    """
+    View to render the Cars/Car hire page
+    """
+
+    return render(request, 'cars/car-hire.html')
