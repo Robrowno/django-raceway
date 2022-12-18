6af1278a17e5090a704e565ee7f424e53549bef2
Christian Brown <robrowno@icloud.com>
2022-12-18 17:46:11 +0000

    create profiles & checkout apps, create cars and contact models in respective apps

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Cars(models.Model):
+
+    FF = 0
+    FR = 1
+    MR = 2
+    AWD = 3
+    RR = 4
+    DRIVETRAINS = [
+        (FF, 'FrontEngine-FrontWheelDrive'),
+        (FR, 'FrontEngine-RearWheelDrive'),
+        (MR, 'MidEngine-RearWheelDrive'),
+        (AWD, 'AllWheelDrive'),
+        (RR, 'RearEngine-RearWheelDrive'),
+    ]
+
+    make = models.CharField(max_length=40, null=True, blank=True)
+    model = models.CharField(max_length=40, null=True, blank=True)
+    variant = models.CharField(max_length=40)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    power = models.PositiveIntegerField()
+    torque = models.PositiveIntegerField()
+    transmission = models.CharField(max_length=80)
+    drivetrain = models.IntegerField(choices=DRIVETRAINS, default=None)
+    cost_to_hire = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return str(self.title)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+QUERY_TYPE = (
+    (0, "General"),
+    (1, "Trackdays/Experiences"),
+    (2, "Bookings"),
+    (3, "Other")
+    )
+
+
+class Contact(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=80)
+    email = models.EmailField()
+    query_type = models.CharField(max_length=11, choices=QUERY_TYPE, default=0)
+    message = models.TextField()
+    contacted_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-contacted_on']
+
+    def __str__(self):
+        return f"{self.full_name} contacted you."
