c0694631c7c31272bd13ddc54e9deb6490f74c3d
Christian Brown <robrowno@icloud.com>
2022-12-30 16:27:59 +0000

    develop trackday details page and test data retrieval from the view, add image to trackday model

--- trackdays/urls.py
+++ trackdays/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.track_day_list, name='trackdays'),
-    path('trackday-booking/', views.track_day_detail, name='booking'),
+    path('trackday-booking/<trackday_id>/', views.track_day_detail, name='booking'),
     path('trackday-request/', views.track_day_request, name='request'),
     path('experiences/', views.experiences, name='experiences'),
     path('experiences/<str:pk>/', views.experiences_detail, name='exp_detail'),
