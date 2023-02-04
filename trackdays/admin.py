9bea84b87147d8b99a4b13a0533a9ffc07695f39
Robrowno <robrowno@icloud.com>
2023-02-04 18:08:08 +0500

    make small changes to fields in models and register trackday and book… …ings models in admin

--- trackdays/admin.py
+++ trackdays/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Trackday, TrackdayBooking
 
 # Register your models here.
+
+
+@admin.register(Trackday)
+class TrackdayAdmin(admin.ModelAdmin):
+
+    """
+    Admin Page for the Trackday model.
+    """
+    list_display = ('date', 'layout',)
+
+
+@admin.register(TrackdayBooking)
+class TrackdayBookingAdmin(admin.ModelAdmin):
+
+    """
+    Admin Page for the Trackday Bookings model.
+    """
+    list_display = ('trackday', 'full_or_half_day',)
