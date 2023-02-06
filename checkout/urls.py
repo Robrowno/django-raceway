82a6f2500de5eafa22625112614086c5f4281882
Robrowno <robrowno@icloud.com>
2023-02-07 01:00:23 +0500

    add to bag views for tuition and experiences, implement forms in deta… …il pages along with associated urls. Begin testing

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('basket/', views.basket, name='basket'),
-    path('checkout-success', views.checkout_success, name='success'),
-
+    path('basket/add/<experience_id>/', views.add_exp_to_basket, name='add_exp_to_basket'),
+    path('basket/add/<tuition_id>/', views.add_tuition_to_basket, name='add_tuition_to_basket'),
+    path('checkout-success/', views.checkout_success, name='success'),
 
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -9,6 +9,44 @@ def basket(request):
     """
 
     return render(request, 'checkout/basket.html')
+
+
+def add_exp_to_basket(request):
+    """
+    For adding an experience package to the basket.
+    """
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if experience_id in list(basket.keys()):
+        if experience_id in list(bag.keys()):
+            basket[experience_id] += quantity
+    else:
+        basket[experience_id] = quantity
+
+    request.session['basket'] = basket
+    print(request.session['basket'])
+    return redirect(redirect_url)
+
+
+def add_tuition_to_basket(request):
+    """
+    For adding a tuition course to the basket.
+    """
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if tuition_id in list(basket.keys()):
+        if tuition_id in list(bag.keys()):
+            basket[tuition_id] += quantity
+    else:
+        basket[tuition_id] = quantity
+
+    request.session['basket'] = basket
+
+    return redirect(redirect_url)
 
 
 def checkout(request):
