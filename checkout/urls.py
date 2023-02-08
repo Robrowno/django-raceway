e89709e37fdb8433131ca34560a5eaf0ed209f62
Robrowno <robrowno@icloud.com>
2023-02-08 09:20:34 +0500

    complete delete functionality by ammending views, urls and js script … …in the basket template

--- checkout/urls.py
+++ checkout/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('basket/add/tuition/<tuition_id>/', views.add_tuition_to_basket, name='add_tuition_to_basket'),
     path('basket/adjust/experience/<item_id>/', views.edit_exp_quantity, name='edit_exp'),
     path('basket/adjust/tuition/<item_id>/', views.edit_tuition_quantity, name='edit_tuition'),
+    path('basket/remove/experience/<item_id>/', views.remove_exp, name='rmv_exp'),
+    path('basket/remove/tuition/<item_id>/', views.remove_tuition, name='rmv_tuition'),
     path('checkout-success/', views.checkout_success, name='success'),
 
 ]
