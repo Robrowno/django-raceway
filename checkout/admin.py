24ed3ff0118253c9429cfbe8df2f2eabe9dab49d
Christian Brown <robrowno@icloud.com>
2023-02-13 10:05:52 +0000

    convert file paths for static images into static django template format for deployment

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import OrderItem
 
 # Register your models here.
+
+
+# @admin.register(OrderItem)
+# class OrderAdmin(admin.ModelAdmin):
+
+#     """
+#     Admin for viewing orders.
+#     """
+#     list_display = ('user_profile', 'date',)
