7c6e7eb117b0471db2b908e1329c7ee112353aa4
Robrowno <robrowno@icloud.com>
2023-02-15 19:43:33 +0500

    latest stripe configurations

--- django_raceway/urls.py
+++ django_raceway/urls.py
@@ -1,24 +1,11 @@
-"""django_raceway URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from checkout.views import success,checkout,cancel,error
 from django.shortcuts import render
 from django.views.defaults import page_not_found
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,8 +16,10 @@ urlpatterns = [
     path('cars/', include('cars.urls')),
     path('profile/', include('profiles.urls')),
     path('checkout/', include('checkout.urls')),
+    # path('success/',success,'success'),
+    # path('checkout/',checkout,'checkout'),
+    #  path('cancel/', cancel, name='cancel'),
+    # path('error/',error, name='error')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 handler404 = 'home.views.page_not_found'
-
